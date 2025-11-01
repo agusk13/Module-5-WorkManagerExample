61975ac2683355848a63e9f445c6b810eaf28a91
agusk13 <agusk1308@gmail.com>
2025-11-01 15:24:50 +0700

    WorkerManagerExample

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.work)
+    implementation(libs.androidx.work.ktx)
+    implementation(libs.airbnb.lottie)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
